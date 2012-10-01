44cb5c589258c1deb632c049fd9c720027bc3e9d
xiaoronglv <xiaoronglv@hotmail.com>
2012-10-01 11:08:50 +0800

    JS  Please enter the commit message for your changes. Lines starting

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null, {code:"var e = document.createElement('script');e.setAttribute('type', 'text/javascript');e.setAttribute('charset', 'UTF-8');e.setAttribute('src', 'http://assets.dxycdn.com/app/pubmed/js/pubmedmarklet.js?r=' + Math.random() * 99999999);document.body.appendChild(e)"});
 });
+// ceshi
